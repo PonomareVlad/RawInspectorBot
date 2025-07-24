38e85e582e1f855d6f754768caa3b66085d2f6c0
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2025-07-24 02:58:42 +0200

    - Enable Edge runtime in `webhook.mjs` configuration - Add timeout for webhook callback - Introduce `@grammyjs/parse-mode` for rich text formatting - Update bot handler with enhanced error boundary and JSON reply formatting - Update dependencies and lockfile

--- api/webhook.mjs
+++ api/webhook.mjs
@@ -3,10 +3,9 @@
 import { bot, secretToken } from '../src/bot.mjs'
 import { webhookCallback } from 'grammy'
 
-// Uncomment next line to use Edge Runtime
-// export const config = { runtime: 'edge' }
+export const config = { runtime: 'edge' }
 
-// Default grammY handler for incoming updates via webhooks
 export const POST = webhookCallback(bot, 'std/http', {
+    timeoutMilliseconds: 24_000,
     secretToken,
 })
--- src/bot.mjs
+++ src/bot.mjs
@@ -1,17 +1,17 @@
 import { Bot } from 'grammy'
+import { fmt, pre } from '@grammyjs/parse-mode'
 
 export const {
-
-    // Telegram bot token from t.me/BotFather
     TELEGRAM_BOT_TOKEN: token,
-
-    // Secret token to validate incoming updates
     TELEGRAM_SECRET_TOKEN: secretToken = String(token).split(':').pop(),
-
 } = process.env
 
-// Default grammY bot instance
 export const bot = new Bot(token)
 
-// Sample handler for a simple echo bot
-bot.on('message:text', ctx => ctx.reply(ctx.msg.text))
+const safe = bot.errorBoundary(console.error)
+
+safe.on('msg', ctx => {
+    const json = JSON.stringify(ctx.update, null, 2)
+    const { text, entities } = fmt`${pre('json')}${json}${pre}`
+    return ctx.reply(text, { entities })
+})
